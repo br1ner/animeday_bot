fed8d578b88da15ceab17cb5a3e626cc0c933e18
br1ner <95130802+br1ner@users.noreply.github.com>
2022-08-23 19:54:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-const http = require('http'),
-    url = require('url');
-
-http.createServer((request, response) => {
-    console.log('create server');
-        let parseUrl = url.parse(request.url, true);
-        sendMsg(parseUrl.query)
-        response.end('good');
-}).listen(3000, 'testrctprjct.herokuapp.com');
-
-const sendMsg = async ({title, episode}) => {
-    await bot.sendMessage(814253143, `
-        Аниме ${title}
-Вышла ${episode} серия
-    `);
-}
 const TelegramApi = require('node-telegram-bot-api'),
     token = "5631146732:AAFTU0cMlDKwbQt7WccxVLU6pVIx5c_M7Fw";
-
 const bot = new TelegramApi(token, { polling: true });
 const start = () => {
     bot.setMyCommands([
@@ -33,3 +16,30 @@ const start = () => {
 }
 
 start()
+const express = require('express'),
+    url = require('url');
+let app = express();
+
+app.get('/', (res, req) => {
+    console.log(res.query);
+    sendMsg(res.query)
+})
+
+let server = app.listen(process.env.PORT || 5000, () => {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log('Web server started at http://%s:%s', host, port);
+})
+// http.createServer((request, response) => {
+//     console.log('create server');
+
+//         response.end('good');
+// }).listen(3000, 'testrctprjct.herokuapp.com');
+
+const sendMsg = async ({title, episode}) => {
+    await bot.sendMessage(814253143, `
+        Аниме ${title}
+Вышла ${episode} серия
+    `);
+}
